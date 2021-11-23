d65715b727e9a5d6895806bd54f99b4da9ebc50e
yidongdeng <248244142@qq.com>
2021-11-23 16:01:17 +0800

    protocol

--- internal/protocol/register.go
+++ internal/protocol/register.go
@@ -11,7 +11,21 @@ func init() {
 
 	pb.Register("im", &proto.CreateConversationReq{}, uint16(proto.CmdType_CmdCreateConversationReq))
 	pb.Register("im", &proto.CreateConversationResp{}, uint16(proto.CmdType_CmdCreateConversationResp))
+	pb.Register("im", &proto.GetConversationUsersReq{}, uint16(proto.CmdType_CmdGetConversationUsersReq))
+	pb.Register("im", &proto.GetConversationUsersResp{}, uint16(proto.CmdType_CmdGetConversationUsersResp))
+
+	pb.Register("im", &proto.AddMemberReq{}, uint16(proto.CmdType_CmdAddMemberReq))
+	pb.Register("im", &proto.AddMemberResp{}, uint16(proto.CmdType_CmdAddMemberResp))
+	pb.Register("im", &proto.RemoveMemberReq{}, uint16(proto.CmdType_CmdRemoveMemberReq))
+	pb.Register("im", &proto.RemoveMemberResp{}, uint16(proto.CmdType_CmdRemoveMemberResp))
+	pb.Register("im", &proto.JoinReq{}, uint16(proto.CmdType_CmdJoinReq))
+	pb.Register("im", &proto.JoinResp{}, uint16(proto.CmdType_CmdJoinResp))
+	pb.Register("im", &proto.QuitReq{}, uint16(proto.CmdType_CmdQuitReq))
+	pb.Register("im", &proto.QuitResp{}, uint16(proto.CmdType_CmdQuitResp))
+	pb.Register("im", &proto.NotifyMemberJoined{}, uint16(proto.CmdType_CmdNotifyMemberJoined))
+	pb.Register("im", &proto.NotifyMemberLeft{}, uint16(proto.CmdType_CmdNotifyMemberLeft))
 	pb.Register("im", &proto.NotifyInvited{}, uint16(proto.CmdType_CmdNotifyInvited))
+	pb.Register("im", &proto.NotifyKicked{}, uint16(proto.CmdType_CmdNotifyKicked))
 
 	pb.Register("im", &proto.SendMessageReq{}, uint16(proto.CmdType_CmdSendMessageReq))
 	pb.Register("im", &proto.SendMessageResp{}, uint16(proto.CmdType_CmdSendMessageResp))
