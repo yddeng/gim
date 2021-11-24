c15038dcba5e82798b92567cfa07132b532535ed
yidongdeng <248244142@qq.com>
2021-11-24 10:51:24 +0800

    gateway

--- pkg/gate/gate.go
+++ pkg/gate/gate.go
@@ -10,39 +10,30 @@ import (
 func StartTCPGateway(address string) error {
 	log.Infof("start tcp gateway on address:%s. ", address)
 	return dnet.ServeTCPFunc(address, func(conn net.Conn) {
-		log.Debug("new client", conn.RemoteAddr().String())
-
-		_ = dnet.NewTCPSession(conn,
-			//dnet.WithTimeout(time.Second*5, 0), // 超时
-			dnet.WithCodec(codec.NewCodec("im")),
-			//dnet.WithErrorCallback(func(session dnet.Session, err error) {
-			//	fmt.Println("onError", err)
-			//}),
-			dnet.WithMessageCallback(func(session dnet.Session, data interface{}) {
-				dispatchMessage(session, data.(*codec.Message))
-			}),
-			dnet.WithCloseCallback(func(session dnet.Session, reason error) {
-				onSessionClose(session, reason)
-			}))
+		log.Debug("new tcp client", conn.RemoteAddr().String())
+		_ = createSession(conn)
 	})
 }
 
 func StartWSGateway(address string) error {
 	log.Infof("start ws gateway on address:%s. ", address)
 	return dnet.ServeWSFunc(address, func(conn net.Conn) {
-		log.Debug("new client", conn.RemoteAddr().String())
-
-		_ = dnet.NewTCPSession(conn,
-			//dnet.WithTimeout(time.Second*5, 0), // 超时
-			dnet.WithCodec(codec.NewCodec("im")),
-			//dnet.WithErrorCallback(func(session dnet.Session, err error) {
-			//	fmt.Println("onError", err)
-			//}),
-			dnet.WithMessageCallback(func(session dnet.Session, data interface{}) {
-				dispatchMessage(session, data.(*codec.Message))
-			}),
-			dnet.WithCloseCallback(func(session dnet.Session, reason error) {
-				onSessionClose(session, reason)
-			}))
+		log.Debug("new ws client", conn.RemoteAddr().String())
+		_ = createSession(conn)
 	})
+}
+
+func createSession(conn net.Conn) dnet.Session {
+	return dnet.NewTCPSession(conn,
+		//dnet.WithTimeout(time.Second*5, 0), // 超时
+		dnet.WithCodec(codec.NewCodec("im")),
+		//dnet.WithErrorCallback(func(session dnet.Session, err error) {
+		//	fmt.Println("onError", err)
+		//}),
+		dnet.WithMessageCallback(func(session dnet.Session, data interface{}) {
+			dispatchMessage(session, data.(*codec.Message))
+		}),
+		dnet.WithCloseCallback(func(session dnet.Session, reason error) {
+			onSessionClose(session, reason)
+		}))
 }
